8153e1a9176629a6a051f0549d1800ed9444e515
Daniel Visicatto <dpfreestyle@gmail.com>
2023-05-02 08:27:23 -0300

    tirado construtor não necessário

--- AndreTurismoApp.Models/Address.cs
+++ AndreTurismoApp.Models/Address.cs
@@ -13,18 +13,6 @@ namespace AndreTurismoApp.Models
         public City City { get; set; }
         public string? ZipCode { get; set; }
         public DateTime RegisterDate { get; set; }
-        #endregion
-
-        #region[Ctor]
-        //public Address(AddressDTO dtoAddress)
-        //{
-        //    Street = dtoAddress.Street;            
-        //    Neighborhood = dtoAddress.Neighborhood;
-        //    Complement = dtoAddress.Complement;
-        //    City.Description = dtoAddress.City;
-        //    ZipCode = dtoAddress.ZipCode;
-        //    RegisterDate = DateTime.Now;
-        //}
-        #endregion
+        #endregion       
     }
 }
